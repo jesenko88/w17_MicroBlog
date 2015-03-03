7d8910e3d6c762a861bad7e6d32db759e9c3aa60
Jesenko <jeshko2@hotmail.com>
2015-03-03 15:35:24 +0100

    Session class,getUsername

--- app/controllers/Session.java
+++ app/controllers/Session.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import models.User;
 import play.mvc.Http.Context;
 import play.mvc.Security;
 
@@ -11,6 +12,10 @@ public class Session extends Security.Authenticator {
 	
 	public String getUsername(Context ctx){
 		Long id = Long.parseLong(ctx.session().get("user_id"));
+		User u = User.find(id);
+		if ( u != null){
+			return u.email;
+		}
 		return null;
 	}
 
